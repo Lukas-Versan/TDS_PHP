03743a321975c910fc1e98cb4406c5552b8608fd
Lukas-Versan <109805479+Lukas-Versan@users.noreply.github.com>
2025-05-06 16:53:29 -0300

    Add files via upload

---
+++ exercícios/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <title>Cálculo de IMC</title>
+    </head>
+    <body>
+        <center><h2>Calculadora IMC</h2></center>
+        <hr/>
+        <div class="row justify-content-center row-cols-1 row-cols-md-2 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
+                    </svg>&nbsp;&nbsp;<font style="font-size: 30px;"><b>Calcule seu IMC</b></font>
+                    </div>
+                    <div class="card-body">
+                        <form action="resultado.php" method="post">
+                            <label class="form-label">Digite seu peso em quilogramas:</label>
+                            <input class="form-control" type="" name="peso" step="0.01" required placeholder="Digite seu peso..."/>
+                            <br/>
+                            <label class="form-label">Digite sua altura em metros:</label>
+                            <input class="form-control" type="" name="altura" step="0.01" required placeholder="Digite sua altura..."/>
+                            <br/>
+                            <input type="submit" class="btn btn-outline-success" value="CALCULAR"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
