582e9e8a0761f7e5a6a6535b51cd547560eaf91f
Lukas-Versan <109805479+Lukas-Versan@users.noreply.github.com>
2025-05-15 17:25:57 -0300

    Add files via upload

---
+++ arrayphp/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale-1">
+        <meta http-equiv="content-language" content="pt-br">
+        <title>PHP / Array</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <style>
+        body{
+            background: linear-gradient(-90deg, #D3D3D3, #363636);
+        }
+    </style>
+    <body>
+        <div class="card-body" style="background: #D3D3D3">    
+            <center><h2><b>PHP/ARRAY</b></h2></center>
+        </div>
+        <br/><br/>
+        <div class="row justify-content-center row-cols-1 row-cols-md-3 text-center">
+            <div class="cols">
+                <div class="card mb-4 rounded shadow-sw">
+                    <div class="card-header py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000080" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+                    <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    </svg>
+                        <h3><font color="black"><b>LOGIN</b></font></h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="login.php" method="post" class="text-start">
+                            <label class="form-label">E-MAIL</label>
+                            <input class="form-control" type="email" name="email" required placeholder="Digite o seu email">
+                            <br/>
+                            <label class="form-label">SENHA</label>
+                            <input class="form-control" type="text" name="senha" required placeholder="Digite a sua senha">
+                            <br/>
+                            <input type="submit" class="btn btn-primary" value="ENTRAR">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
