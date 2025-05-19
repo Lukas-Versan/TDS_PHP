efb281d38d2e6fd688b0405987200ff3244a1ef8
Lukas-Versan <109805479+Lukas-Versan@users.noreply.github.com>
2025-05-19 19:56:12 -0300

    Add files via upload

--- arrayphp/index.php
+++ arrayphp/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -13,7 +12,7 @@
             float: right;
         }
         body{
-            background: linear-gradient(-45deg, #D3D3D3, #A9A9A9, #363636, #1C1C1C);
+            background: linear-gradient(-45deg, #4F4F4F, #363636, #363636, #1C1C1C);
             background-size: 400% 400%;
             animation: gradient 15s ease infinite;
         }
@@ -32,6 +31,107 @@
         .card-body{
             background-color: #D3D3D3;
         }
+        /* From Uiverse.io by 3bdel3ziz-T */ 
+        .card {
+            --main-col: #ffeba7;
+            --bg-col: #2a2b38;
+            --bg-field: #1f2029;
+
+            width: 500px;
+            padding: 1.9rem 1.2rem;
+            text-align: center;
+            background: var(--bg-col);
+            border-radius: 10px;
+            border: 1px solid var(--main-col);
+            user-select: none;
+        }
+
+        /*Inputs*/
+        .field {
+            margin-top: 0.5rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding-left: 0.5rem;
+            gap: 0.5rem;
+            background-color: var(--bg-field);
+            border-radius: 4px;
+        }
+
+        .input-icon {
+            width: 1em;
+            color: var(--main-col);
+            fill: var(--main-col);
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .input-field {
+            background: transparent;
+            border: none;
+            outline: none;
+            width: 100%;
+            color: var(--main-col);
+            padding: 0.5em 1em 0.5em 0;
+            caret-color: var(--main-col);
+        }
+
+        .filed:has(.input-field:valid) {
+            border: 1px solid var(--main-col);
+        }
+
+        /*Text*/
+        .title {
+            margin-bottom: 1rem;
+            font-size: 1.5em;
+            font-weight: 500;
+            color: var(--main-col);
+            text-shadow: 1px 1px 20px var(--main-col);
+            text-transform: uppercase;
+        }
+
+        /*Buttons*/
+        .btn {
+            margin: 1rem;
+            border: none;
+            border-radius: 4px;
+            font-weight: bold;
+            font-size: 0.8em;
+            text-transform: uppercase;
+            padding: 0.6em 1.2em;
+            background-color: var(--main-col);
+            color: var(--bg-col);
+            box-shadow: 0 8px 24px 0 rgb(255 235 167 / 20%);
+            transition: all 0.3s ease-in-out;
+            cursor: pointer;
+        }
+
+        .btn-link {
+            color: #f5f5f5;
+            display: block;
+            font-size: 0.75em;
+            transition: color 0.3s ease-out;
+        }
+
+        /*Hover & focus*/
+        .field input:focus::placeholder {
+            opacity: 0;
+            transition: opacity 0.3s;
+        }
+
+        .btn:hover {
+            background-color: var(--bg-field);
+            color: var(--main-col);
+            box-shadow: 0 8px 24px 0 rgb(16 39 112 / 20%);
+        }
+
+        .btn-link:hover {
+            color: var(--main-col);
+            text-decoration: underline;
+        }
+
     </style>
     <body>
         <div class="card-body" style="background-color: #D3D3D3">    
@@ -40,26 +140,42 @@
         <br/><br/>
         <div class="row justify-content-center row-cols-1 row-cols-md-3 text-center">
             <div class="cols">
-                <div class="card mb-4 rounded shadow-sw">
-                    <div class="card-header py-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000080" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
-                    <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                    </svg>
-                        <h3><font color="black"><b>LOGIN</b></font></h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="login.php" method="post" class="text-start">
-                            <label class="form-label">E-MAIL</label>
-                            <input class="form-control" type="email" name="email" required placeholder="Digite o seu email">
-                            <br/>
-                            <label class="form-label">SENHA</label>
-                            <input class="form-control" type="text" name="senha" required placeholder="Digite a sua senha">
-                            <br/>
-                            <input type="submit" class="btn btn-primary" value="ENTRAR">
+                    <div class="card">
+                        <h4 class="title">Login</h4>
+                        <form action="login.php" method="post">
+                            <label class="field" for="logemail">
+                            <span class="input-icon">@</span>
+                            <input
+                                autocomplete="off"
+                                id="logemail"
+                                placeholder="Email"
+                                class="input-field"
+                                name="email"
+                                type="email"
+                            />
+                            </label>
+                            <label class="field" for="logpass">
+                            <svg
+                                class="input-icon"
+                                viewBox="0 0 500 500"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"
+                                ></path>
+                            </svg>
+
+                            <input
+                                id="logpass"
+                                placeholder="Senha"
+                                class="input-field"
+                                name="senha"
+                                type="password"
+                            />
+                            </label>
+                            <button class="btn" type="submit">Entrar</button>
                         </form>
                     </div>
-                </div>
             </div>
         </div>
     </body>
--- arrayphp/inicial.php
+++ arrayphp/inicial.php
@@ -24,7 +24,7 @@
             float: right;
         }
         body{
-            background: linear-gradient(-45deg, #D3D3D3, #A9A9A9, #363636, #1C1C1C);
+            background: linear-gradient(-45deg, #4F4F4F, #363636, #363636, #1C1C1C);
             background-size: 400% 400%;
             animation: gradient 18s ease infinite;
         }
@@ -43,6 +43,50 @@
         .card-body{
             background-color: #D3D3D3;
         }
+        /* From Uiverse.io by satyamchaudharydev */ 
+        /* === removing default button style ===*/
+        .button {
+            margin: 0;
+            height: auto;
+            background: transparent;
+            padding: 0;
+            border: none;
+            cursor: pointer;
+        }
+
+        /* button styling */
+        .button {
+            --border-right: 6px;
+            --text-stroke-color: rgba(255,255,255,0.6);
+            --animation-color: #37FF8B;
+            --fs-size: 2em;
+            letter-spacing: 3px;
+            text-decoration: none;
+            font-size: var(--fs-size);
+            font-family: "Arial";
+            position: relative;
+            text-transform: uppercase;
+            color: transparent;
+            -webkit-text-stroke: 1px var(--text-stroke-color);
+        }
+        /* this is the text, when you hover on button */
+        .hover-text {
+            position: absolute;
+            box-sizing: border-box;
+            content: attr(data-text);
+            color: var(--animation-color);
+            width: 0%;
+            inset: 0;
+            border-right: var(--border-right) solid var(--animation-color);
+            overflow: hidden;
+            transition: 0.5s;
+            -webkit-text-stroke: 1px var(--animation-color);
+        }
+        /* hover */
+        .button:hover .hover-text {
+            width: 100%;
+            filter: drop-shadow(0 0 23px var(--animation-color))
+        }
     </style>
     <body>
         <div class="card-body" style="background-color: #D3D3D3">    
@@ -56,9 +100,12 @@
             </div>
         </nav>
         <br/><br/>
-        <center><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <b>CADASTRAR NOVO USUÁRIO</b>
-        </button></center>
+        <center>
+            <button class="button" data-text="Awesome" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <span class="actual-text">&nbsp;CADASTRAR&nbsp;</span>
+                <span aria-hidden="true" class="hover-text">&nbsp;CADASTRAR&nbsp;</span>
+            </button>
+        </center>
         <br/><br/>
         <div class="row justify-content-center row-cols-2 row-cols-md-3 text-center">
             <div class="cols">
